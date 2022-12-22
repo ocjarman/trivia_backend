9ea36a709b107fef3c49fc90bfef98abddd495c0
ocjarman <ocjarman@gmail.com>
2022-12-22 14:14:01 -0500

    messages coming through on both ends

--- server/index.js
+++ server/index.js
@@ -1,12 +1,52 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const path = require("path");
+const volleyball = require("volleyball");
 
-const init = async () => {
-  try {
-    const port = process.env.PORT || 4000;
-    app.listen(port, () => console.log(`listening on port ${port}`));
-  } catch (ex) {
-    console.log(ex);
-  }
-};
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
-init();
+// need cors to make connection with frontend
+app.use(cors());
+
+const server = http.createServer(app);
+
+// allows us to work with socket.io
+const io = new Server(server, {
+  cors: {
+    // this is the origin of the REACT app
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+// Set up middleware
+app.use(volleyball);
+app.use(express.json());
+
+//sets up body for us when making post requests or any req that needs a body
+// app.use(express.urlencoded({ extended: true }));
+
+// Set up routes
+app.use("/api", require("./api"));
+
+// when someone goes to connect to server thru client, this will start running
+io.on("connection", (socket) => {
+  // socket id is player id
+  console.log(`Player is connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    // take the data received and broadcasts to others
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("server is running");
+});
+
+// module.exports = server;
