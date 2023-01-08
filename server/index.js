dfbd153b11c658f3319ab892d333673650dd114a
ocjarman <ocjarman@gmail.com>
2023-01-08 13:24:45 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // this is the origin of the REACT app
-    origin: "http://localhost:3000",
+    origin: "https://ocjarman.github.io/trivia_frontend/",
     methods: ["GET", "POST"],
   },
 });
